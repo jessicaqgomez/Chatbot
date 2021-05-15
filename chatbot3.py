883a92bc88da4564c043726d887512970d0eea5f
jessicaqgomez <jessica.1701610723@ucaldas.edu.co>
2021-05-14 22:05:47 -0500

    Create chatbot3.py

---
+++ chatbot3.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# pylint: disable=C0116
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+from typing import Dict
+
+from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+reply_keyboard = [
+    ['Registrarse', 'Consultar'],
+    [ 'Something else...'],
+    ['Done'],
+]
+
+reply_keyboard2 = [['registrarse','consultar']]
+
+
+markup = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True)
+
+
+def facts_to_str(user_data: Dict[str, str]) -> str:
+    facts = list()
+
+    for key, value in user_data.items():
+        facts.append(f'{key} - {value}')
+
+    return "\n".join(facts).join(['\n', '\n'])
+
+
+def start(update: Update, _: CallbackContext) -> int:
+    update.message.reply_text(
+        "Hola, soy tu profesor de la universidad de pitlover, como te llamas? escribe la respuesta comenzando con /nombre y tu nombre",
+        reply_markup=markup,
+    )
+
+    return CHOOSING
+
+def saludo( update: Update, context:CallbackContext):
+    mensaje = str(context.args[0])
+    print(mensaje)
+    if (any(char.isdigit() for char in mensaje)):
+        update.message.reply_text("tu nombre contiene elementos no validos",reply_markup=markup)
+        
+    else:
+        resp= "hola "+mensaje.lower()+" en qué puedo ayudarte"
+        update.message.reply_text(resp,reply_markup=markup)
+    return CHOOSING
+
+
+def regular_choice(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    context.user_data['choice'] = text
+    update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
+
+    return TYPING_REPLY
+
+
+def custom_choice(update: Update, _: CallbackContext) -> int:
+    update.message.reply_text(
+        'Alright, please send me the category first, for example "Most impressive skill"'
+    )
+
+    return TYPING_CHOICE
+
+
+def search_information(codigo):
+    estado = calificado
+    return estado
+
+def received_information(update: Update, context: CallbackContext) -> int:
+    user_data = context.user_data
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text(
+        "Neat! Just so you know, this is what you already told me:"
+        f"{facts_to_str(user_data)} You can tell me more, or change your opinion"
+        " on something.",
+        reply_markup=markup,
+    )
+
+    return CHOOSING
+
+
+def done(update: Update, context: CallbackContext) -> int:
+    user_data = context.user_data
+    if 'choice' in user_data:
+        del user_data['choice']
+
+    update.message.reply_text(
+        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    user_data.clear()
+    return ConversationHandler.END
+
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("1884200283:AAHZa1nA3nyTTIdFC1lMQn9lGGaMjCrn-JM")
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start',start))
+    #dispatcher.add_handler(MessageHandler(Filters.update,saludo))
+    
+    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('nombre',saludo)],
+        states={
+            CHOOSING: [
+                MessageHandler(
+                    Filters.regex('^(Registrarse|consultar)$'), regular_choice
+                ),
+                MessageHandler(Filters.regex('^Something else...$'), custom_choice),
+            ],
+            TYPING_CHOICE: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Done$')), regular_choice
+                )
+            ],
+            TYPING_REPLY: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Done$')),
+                    received_information,
+                )
+            ],
+        },
+        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
